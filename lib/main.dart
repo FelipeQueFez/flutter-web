4d216166728105d4557a4cde671b15b891c0ab30
“FelipeQueFez“ <felipequefez@gmail.com>
2024-10-22 18:17:22 +0100

    chore:3.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCwoPvRFTMnjoKFDr21uxyCNaCw3FhvlPE",
+      appId: "1:514779891364:web:210eec466b0db31b844831",
+      messagingSenderId: "514779891364",
+      projectId: "image-gallery-web",
+    ),
+  );
+
   runApp(MyApp());
 }
 
@@ -10,37 +21,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Image gallery app',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image gallery 2'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Image gallery web'),
+        ),
+        body: Center(
+          child: Text('Firebase initialized succesfully'),
+        ),
       ),
-      body: LayoutBuilder(builder: (context, constraints) {
-        if (constraints.maxWidth < 600) {
-          return ListView(
-            children: buildImageList(),
-          );
-        } else {
-          return GridView.count(
-            crossAxisCount: 3,
-            children: buildImageList(),
-          );
-        }
-      }),
     );
-  }
-
-  List<Widget> buildImageList() {
-    return List.generate(10, (index) {
-      return Image.network(
-          'https://cdn.evolve-mma.com/wp-content/uploads/2015/05/why-bjj-is-the-perfect-martial-art.jpg');
-    });
   }
 }
