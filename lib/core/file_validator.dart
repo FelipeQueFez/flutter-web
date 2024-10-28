92d12c3dc11d874c483ec03a37a6b98539141e0b
“FelipeQueFez“ <felipequefez@gmail.com>
2024-10-28 19:09:24 +0000

    chore:unit test

---
+++ lib/core/file_validator.dart
@@ -0,0 +1,26 @@
+import 'dart:typed_data';
+
+class FileValidator {
+  static const int maxFileSize = 5 * 1024 * 1024;
+  static const String validExtension = '.jpeg';
+
+  static bool isJpeg(String filename) {
+    return filename.toLowerCase().endsWith(validExtension);
+  }
+
+  static bool isFileSizeValid(Uint8List fileBytes) {
+    return fileBytes.length <= maxFileSize;
+  }
+
+  static bool validateFile(Uint8List fileBytes, String filename) {
+    if (!isJpeg(filename)) {
+      return false;
+    }
+
+    if (!isFileSizeValid(fileBytes)) {
+      return false;
+    }
+
+    return true;
+  }
+}
